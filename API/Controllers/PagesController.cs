5f3492f3e2994906f78cb1594c982073d7417f73
ProfessorLagg <futurecake2999@gmail.com>
2024-09-23 11:07:31 +0200

    Begun doing the auth myself

---
+++ API/Controllers/PagesController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers {
+    [Route("page")]
+    [ApiController]
+    public class PagesController : ControllerBase {
+
+    }
+}
---
+++ Database/StoredProcedures/UserSessions.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[UserSessions]
+	@userId int,
+	@onlyActive bit = 0
+AS
+IF @onlyActive = 1
+	SELECT * FROM [Sessions] WHERE [Sessions].[UserId] = @userId AND [Sessions].[Active] = 1
+ELSE
+	SELECT * FROM [Sessions] WHERE [Sessions].[UserId] = @userId 
+RETURN 0
